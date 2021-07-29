f960558f4af7ec96cfe241dd3c3c86e2b61be307
Mariana Fernandez <mferna13@lesley.edu>
2021-07-29 18:53:54 -0400

    Create rings.py
    A semi-abstract piece using coding principles from the book and from Python.org. code took me about 17 hours to do as I kept messing up and not getting the desired result. I had to look up on Python.org how to fix my many mistakes. This included some forgetting of () which python wasn't giving me error for but ran without. also looping numbers N=100 had to go outside of #for i in range(): and the brackets for lists of data.

---
+++ rings.py
@@ -0,0 +1,52 @@
+import turtle
+lucid=turtle.Turtle()
+lucid.speed("fastest")
+turtle.bgcolor("slate grey")
+
+# ring positions, colors, and shapes
+NumShapes = 5
+Xpos = [-100, -50, 0, 50, 100]
+Ypos = [50, 0, 50, 0, 50]
+Colors = ["blue", "gold", "black", "green", "red"]
+Angles = [90,64,74,80,50]
+
+# draw rings
+for ShapeNo in range(NumShapes):
+    lucid.color(Colors[ShapeNo])
+    lucid.penup()
+    lucid.goto(Xpos[ShapeNo], Ypos[ShapeNo])
+    lucid.pendown()
+    N=10
+    for i in range(30):
+        lucid.left(Angles[ShapeNo])
+        lucid.forward(N)
+        N=N+2
+
+# draw text
+lucid.penup()
+lucid.goto(0,-40)
+lucid.pendown()
+lucid.color("black")
+# Google for "python turtle write text"
+lucid.write("Tokyo 2020!", font=("Arial", 20, "normal"))
+
+# draw flag
+lucid.penup()
+# https://docs.python.org/3/library/turtle.html#turtle.setheading
+lucid.setheading(0)
+lucid.goto(0,-150)
+lucid.pendown()
+lucid.begin_fill()
+lucid.color("white")
+for i in range(4):
+    lucid.forward(100)
+    lucid.left(90)
+lucid.end_fill()
+lucid.goto(50,-130)
+lucid.begin_fill()
+lucid.color("red")
+lucid.circle(30)
+lucid.end_fill()
+
+lucid.hideturtle()
+turtle.exitonclick()
