6b3f2ffb159201b95c77f3487f87e5d41b3a824a
Mariana Fernandez <mferna13@lesley.edu>
2021-08-29 20:41:55 -0400

    Update Bodypositivenumber2.py
    Updated with extra
    comments

--- Bodypositivenumber2.py
+++ Bodypositivenumber2.py
@@ -1,6 +1,7 @@
 import time
+# Show the welcome message
 print("Welcome to the Body Positivity Questionaire. Please Answer each question with yes or no.")
-#List will help make the questions easier to input each time and the lists allow for afirmations to print concurrently 
+# Store questions and affirmations in lists (concurrently - question 1 corresponds to affirmation 1). This makes our code more flexible as one can add/modify questions without changing the rest of the code.
 Questions = [
     "I don't like certain parts of my body.",
     "I deny my bodies needs regularly.",
@@ -15,7 +16,8 @@ Affirmation=[
     "Others put us down because they don't  feel good about themselves."]
         
 Num_Questions = len(Questions)
-# using functions while loops and boolean operators
+# I'm using functions, while loops, and boolean operators here.
+# This function will ask user a question and prompt them again if they have mistyped their answer (e.g. said maybe instead of yes or no)
 def AskForYesOrNo(question):
     Answer = input(question)
 
@@ -25,10 +27,10 @@ def AskForYesOrNo(question):
         Answer=input(question)
 
     return Answer
-
+# Maintain two counters for the number of yes or no answers encountered during the quiz.
 YesCount = 0
 NoCount = 0
-#using for loops
+# Here I'm using for loops to ask questions one by one
 for i in range(Num_Questions):
     current_question = Questions[i]
     print("Here is a question #", i+1, "for you:")
@@ -37,18 +39,22 @@ for i in range(Num_Questions):
     # at this point Response is guaranteed to be one of "Yes", "yes", "No", "no"
     
     if Response == "Yes" or Response == "yes":
+# if the answer is yes, increment yes counter and print and affirmation
         YesCount = YesCount + 1
         current_affirmtion= Affirmation[i]
         print(current_affirmtion)
     else:
+# otherwise the answer is "no" and increment "no" answer
         NoCount = NoCount + 1
-
+# Display final statistics
 print("number of yes=", YesCount)
 print("number of no=", NoCount)
 
 if YesCount>NoCount:
+    # If yes is the majority statistic and shares a helpful resource
     print("I'm Here to share a resource I find helpful. Lets remember every-body has somebody to care for. Please visit https://positivepsychology.com/positive-body-image/")
 else:
+#The user gets a postive message and encouraged to check in with friends.
     print(" It's great you feel good about your body.Check in on your friends they might be struggling with body positivity!")
 #using turtle  to give code more User Centered design by making poem easier to see
 
